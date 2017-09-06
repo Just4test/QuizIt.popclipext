c3235156229f94285e6ed3b464b2850d9bf266bd
Just4test <myservice@just4test.net>
2017-09-06 13:56:50 +0800

    解决在激活token时显示激活信息而不是单词解释的问题。

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def defaults_storage_read(key, default_value = None):
 	if temp.returncode == 0:
 		return temp.stdout.decode('unicode-escape').rstrip('\n') #turn binary string to string, and remove \n
 	return default_value
-	
+
 def get_oauth_token(code):
 	url = 'https://api.quizlet.com/oauth/token'
 	params = {
@@ -37,14 +37,13 @@ def get_oauth_token(code):
 	auth = requests.auth.HTTPBasicAuth(QUIZLET_CLIENT_ID, QUIZLET_SECRET_KEY)
 	try:
 		r = requests.post(url=url, params=params, auth=auth)
-		print(r.json())
 		if r.status_code == 200:
 			return r.json()
 		return None
 	except requests.exceptions.RequestException as e:
 		print('发生网络错误')
 		exit()
-	
+
 def get_token():
 	AUTH_SUCCESS = '******'
 	code = defaults_storage_read('oauth_code')
@@ -55,16 +54,16 @@ def get_token():
 			exit(2)
 		defaults_storage_write('oauth_code', AUTH_SUCCESS)
 		defaults_storage_write('access_token', data['access_token'])
-		
+
 	token = defaults_storage_read('access_token')
 	if token is None:
 		defaults_storage_write('oauth_code', '请指定此值')
 		exit(2)
-		
+
 	return token
-		
-			
-	
+
+
+
 def add_term(set_id, term, definition):
 	url = 'https://api.quizlet.com/2.0/sets/{}/terms'.format(set_id)
 	params = {
@@ -89,7 +88,7 @@ def add_term(set_id, term, definition):
 	except requests.exceptions.RequestException as e:
 		print('发生网络错误')
 		exit()
-		
+
 def get_word_definition(word):
 	try:
 		r = requests.get('https://api.shanbay.com/bdc/search/',
@@ -100,12 +99,12 @@ def get_word_definition(word):
 		if r['status_code'] != 0:
 			print(r['msg'])
 			exit(0)
-		
+
 		return r['data']['content'], r['data']['definition']
 	except requests.exceptions.RequestException as e:
 		print('发生网络错误')
 		exit()
-	
+
 
 word = os.environ.get('POPCLIP_TEXT', '')
 word, definition = get_word_definition(word)
