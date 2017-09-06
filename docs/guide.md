11c446dcd20c2658d90bd0104b9e5e39241a699c
Just4test <myservice@just4test.net>
2017-09-06 14:51:36 +0800

    改为oauth简化模式，直接获取token。

--- docs/guide.md
+++ docs/guide.md
@@ -2,9 +2,7 @@
 ## 1. Get oauth code
 Click [here](https://quizlet.com/authorize)
 
-Your code is xxxx.
-
-**Please paste it to QuizIt settings and use QuizIt add any word in *1 minute* to active this code.**
+Your access token is xxxx.
 
 ## 2. Get set ID
 Open Quizlet in a browser,  navigate to one of your set, you can find set ID in the address bar.
--- run.py
+++ run.py
@@ -11,9 +11,10 @@ REDIRECT_URL = 'http://htmlpreview.github.io/?https://github.com/Just4test/Quizl
 POPCLIP_BUNDLE_ID = 'com.pilotmoon.popclip'
 EXTENSION_ID = 'net.just4test.popclip.quizit'
 OAUTH_CODE_ENV = 'POPCLIP_OPTION_OAUTH_CODE'
+ACCESS_TOKEN_ENV = 'POPCLIP_OPTION_ACCESS_TOKEN'
 SET_ID_ENV = 'POPCLIP_OPTION_SET_ID'
 
-
+#现在可以从guide页面上直接过去access token了
 def defaults_storage_write(key, value):
 	'使用defaults存储文本'
 	args = ['defaults', 'write', POPCLIP_BUNDLE_ID, 'extension#{}#{}'.format(EXTENSION_ID, key), str(value)]
@@ -27,51 +28,51 @@ def defaults_storage_read(key, default_value = None):
 		return temp.stdout.decode('unicode-escape').rstrip('\n') #turn binary string to string, and remove \n
 	return default_value
 
-def get_oauth_token(code):
-	url = 'https://api.quizlet.com/oauth/token'
-	params = {
-		'code': code,
-		'redirect_uri': REDIRECT_URL,
-		'grant_type': 'authorization_code'
-	}
-	auth = requests.auth.HTTPBasicAuth(QUIZLET_CLIENT_ID, QUIZLET_SECRET_KEY)
-	try:
-		r = requests.post(url=url, params=params, auth=auth)
-		if r.status_code == 200:
-			return r.json()
-		return None
-	except requests.exceptions.RequestException as e:
-		print('发生网络错误')
-		exit()
+#def get_oauth_token(code):
+#	url = 'https://api.quizlet.com/oauth/token'
+#	params = {
+#		'code': code,
+#		'redirect_uri': REDIRECT_URL,
+#		'grant_type': 'authorization_code'
+#	}
+#	auth = requests.auth.HTTPBasicAuth(QUIZLET_CLIENT_ID, QUIZLET_SECRET_KEY)
+#	try:
+#		r = requests.post(url=url, params=params, auth=auth)
+#		if r.status_code == 200:
+#			return r.json()
+#		return None
+#	except requests.exceptions.RequestException as e:
+#		print('发生网络错误')
+#		exit()
+#
+#def get_token():
+#	AUTH_SUCCESS = '******'
+#	code = defaults_storage_read('oauth_code')
+#	if code is not None and code != "" and code != AUTH_SUCCESS:
+#		data = get_oauth_token(code)
+#		if data is None:
+#			defaults_storage_write('oauth_code', '不正确的值')
+#			exit(2)
+#		defaults_storage_write('oauth_code', AUTH_SUCCESS)
+#		defaults_storage_write('access_token', data['access_token'])
+#
+#	token = defaults_storage_read('access_token')
+#	if token is None:
+#		defaults_storage_write('oauth_code', '请指定此值')
+#		exit(2)
+#
+#	return token
 
-def get_token():
-	AUTH_SUCCESS = '******'
-	code = defaults_storage_read('oauth_code')
-	if code is not None and code != "" and code != AUTH_SUCCESS:
-		data = get_oauth_token(code)
-		if data is None:
-			defaults_storage_write('oauth_code', '不正确的值')
-			exit(2)
-		defaults_storage_write('oauth_code', AUTH_SUCCESS)
-		defaults_storage_write('access_token', data['access_token'])
-
-	token = defaults_storage_read('access_token')
-	if token is None:
-		defaults_storage_write('oauth_code', '请指定此值')
-		exit(2)
-
-	return token
 
 
-
-def add_term(set_id, term, definition):
+def add_term(access_token, set_id, term, definition):
 	url = 'https://api.quizlet.com/2.0/sets/{}/terms'.format(set_id)
 	params = {
 		'term': term,
 		'definition': definition
 	}
 	headers = {
-		'Authorization': 'Bearer ' + get_token()
+		'Authorization': 'Bearer ' + access_token
 	}
 	try:
 		r = requests.post(url=url, params=params, headers=headers)
@@ -110,6 +111,23 @@ word = os.environ.get('POPCLIP_TEXT', '')
 word, definition = get_word_definition(word)
 
 set_id = os.environ.get(SET_ID_ENV, '')
-add_term(set_id, word, definition)
+access_token = os.environ.get(ACCESS_TOKEN_ENV, '')
+#以下代码为了调试方便，因为每次重新安装popclipext都会清空配置，所以做一个备份恢复机制
+if set_id == '':
+	set_id = defaults_storage_read('set_id_backup', '')
+	defaults_storage_write('set_id', set_id)
+else:
+	defaults_storage_write('set_id_backup', set_id)
+	
+if access_token == '':
+	access_token = defaults_storage_read('access_token_backup', '')
+	defaults_storage_write('access_token', access_token)
+else:
+	defaults_storage_write('access_token_backup', access_token)
+	
+if set_id == '' or access_token == '':
+	exit(2)
+	
+add_term(access_token, set_id, word, definition)
 
 print(definition.replace('\n', ' '))
